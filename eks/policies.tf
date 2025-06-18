c091995ca964ffb44e680455948b99c9d8a1f184
pavannayakanti <32892454+pavannayakanti@users.noreply.github.com>
2025-06-18 20:17:00 +0530

    Update policies.tf

--- eks/policies.tf
+++ eks/policies.tf
@@ -1,8 +1,7 @@
 resource "aws_iam_policy" "loadbalancer_policy" {
-  name        = var.additional_policy_name
+  name        = local.additional_policy_name
   path        = "/"
   description = "Policy for granting rights to create loadbalancer services and EC2 volumes"
 
   policy = jsonencode(yamldecode(file("./policy.yaml")))
 }
-
